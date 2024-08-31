0249e03f5405cf7be61797ad92cc05e126e63530
thejzain <thejzainkp@gmail.com>
2024-09-01 01:38:58 +0530

    huffman tree constructed

--- src/binarytree.rs
+++ src/binarytree.rs
@@ -3,33 +3,37 @@ pub struct TreeNode<T> {
     value: T,
     left: Option<Box<TreeNode<T>>>,
     right: Option<Box<TreeNode<T>>>,
+    pub occurrence: i32,
 }
 
 impl<T> TreeNode<T> {
-    pub fn new(value: T) -> Self {
+    pub fn new(value: T, occurrence: i32) -> Self {
         TreeNode {
             value,
             left: None,
             right: None,
+            occurrence,
         }
     }
 
-    pub fn insert(&mut self, value: T)
+    pub fn insert(&mut self, leftroot: TreeNode<T>, rightroot: TreeNode<T>)
     where
         T: PartialOrd,
     {
-        if value < self.value {
-            if let Some(left) = &mut self.left {
-                left.insert(value);
-            } else {
-                self.left = Some(Box::new(TreeNode::new(value)));
-            }
-        } else if value > self.value {
-            if let Some(right) = &mut self.right {
-                right.insert(value);
-            } else {
-                self.right = Some(Box::new(TreeNode::new(value)));
-            }
+        if leftroot.occurrence < rightroot.occurrence {
+            self.right = Some(Box::new(leftroot));
+            self.left = Some(Box::new(rightroot));
+        } else {
+            self.right = Some(Box::new(rightroot));
+            self.left = Some(Box::new(leftroot));
         }
     }
+}
+
+pub fn mergetrees(left: TreeNode<String>, right: TreeNode<String>) -> TreeNode<String> {
+    let value = format!("{}{}", right.value, left.value);
+    let occurrence = right.occurrence + left.occurrence;
+    let mut root = TreeNode::new(value, occurrence);
+    root.insert(left, right);
+    root
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 mod binarytree;
 
-use binarytree::TreeNode;
+use binarytree::{mergetrees, TreeNode};
 use std::collections::HashMap;
 use std::env::args;
 use std::process::exit;
@@ -34,16 +34,25 @@ fn main() -> io::Result<()> {
         }
     });
 
-    let mut mapped_vector: Vec<Token> = Vec::new();
+    let mut mapped_vector: Vec<(char, i32)> = Vec::new();
     let mut treenode_vector: Vec<TreeNode<String>> = Vec::new();
 
     for ele in map {
-        mapped_vector.push(Token::new(ele.0.to_string(), ele.1));
+        mapped_vector.push((ele.0, ele.1));
     }
-    mapped_vector.sort_by(|a, b| a.occurrence.partial_cmp(&b.occurrence).unwrap());
+    mapped_vector.sort_by(|a, b| b.1.partial_cmp(&a.1).unwrap());
 
     for token in mapped_vector {
-        treenode_vector.push(TreeNode::new(token.value));
+        treenode_vector.push(TreeNode::new(token.0.to_string(), token.1));
+    }
+
+    while treenode_vector.len() > 1 {
+        let root1 = treenode_vector.pop().unwrap();
+        let root2 = treenode_vector.pop().unwrap();
+
+        let newroot = mergetrees(root1, root2);
+        treenode_vector.push(newroot);
+        treenode_vector.sort_by(|a, b| b.occurrence.partial_cmp(&a.occurrence).unwrap());
     }
     dbg!(treenode_vector);
     Ok(())
